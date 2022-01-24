9640df73a7d4c0de147a547ea59edf4951226853
Edwin Sulaiman <edwinsulaiman20@gmail.com>
2022-01-24 23:08:31 +0700

    Add menu to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
+const Link = (props) => {
+  return <div className="py-2 text-white">{props.label}</div>;
+}
 
 const Navbar = () => {
+  const [isMenuShow, setIsMenuShow] = useState(false);
+
   return (
-    <div className="flex justify-between p-4 bg-indigo-600">
-      <span className="text-white">Navbar</span>
-      <div className="text-white block md:hidden"><MenuIcon /></div>
+    <div className="p-4 bg-indigo-600">
+      <div className="flex justify-between">
+        <span className="text-white">Navbar</span>
+        <div className="text-white block md:hidden" onClick={() => setIsMenuShow(!isMenuShow)}>{isMenuShow ? <CloseIcon /> : <MenuIcon />}</div>
+      </div>
+      {isMenuShow &&
+        <div>
+          <Link label="Home"/>
+          <Link label="About"/>
+          <Link label="Contact"/>
+        </div>
+      }
     </div>
   );
 };
