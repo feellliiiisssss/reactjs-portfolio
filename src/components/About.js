0a9b213e1d5238fd417513cbcfe7af8e740dc9df
felis <feellliiiisssss@yahoo.com>
2022-02-07 22:44:45 +0700

    master

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#0B4619]">
+      <div className="text-white">
+        <p className="md:text-6xl text-4xl text-left font-['Bebas_Neue'] tracking-wide">Let's Talk</p>
+        <p className="md:text-9xl text-7xl text-left font-['Bebas_Neue'] tracking-wide">ABOUT</p>
+        <p className="md:text-9xl text-7xl text-left font-['Bebas_Neue'] tracking-wide">ME</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
